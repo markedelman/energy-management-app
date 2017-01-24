8b9857746afeaa04932c5d0fd400476cacc219e4
Mark Edelman <markedelman1987@gmail.com>
2017-01-23 23:27:03 -0500

    mongo and mongoose connected in server folder  after help from Alex

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-import 'babel-polyfill';
+// import 'babel-polyfill';
+require('babel-polyfill');
 import express from 'express';
-
+// import mongoose from 'mongoose';
+var mongoose  = require('mongoose');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -9,7 +11,45 @@ console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
 const app = express();
 
+// substituting mongoose Promise with Node's due to deprecation
+mongoose.Promise = global.Promise;
+
+// creating the Schema for persistent objects
+var EnergySchema = mongoose.Schema({
+    userValue: {
+        type: Number,
+        required: true
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// creating Model for Mongo to store documents according to the Schema
+var Energy = mongoose.model('energyRecord', EnergySchema);
+
+// Connection to MongoDB through Mongoose
+mongoose.connect('mongodb://localhost/energyManagement');
+mongoose.connection.once('open', ()=> {
+    console.log('Connection is Open')
+}).on('error', () => {
+    console.log('Error in Connection')
+});
+
+// creating a new record in the above DB & Collections
+var entry = new Energy({
+    userValue: 10
+});
+
+// saving the new instance to MongoDB
+entry.save().then((resp) => {
+    console.log(resp)
+});
+
+
 app.use(express.static(process.env.CLIENT_PATH));
+
 
 function runServer() {
     return new Promise((resolve, reject) => {
