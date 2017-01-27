dd16ffc0a704819f796e2a7a8ce5de3e35d7675f
Mark Edelman <markedelman1987@gmail.com>
2017-01-27 01:53:50 -0500

    tried to get reducers working but get error, previous commit worked

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -2,16 +2,40 @@ import 'isomorphic-fetch';
 import { combineReducers } from 'redux';
 const url = 'http://localhost:8080/energy';
 
-var reducer = function(){
-    var state = [];
-    fetch(url)
+
+
+const initialEntryState = [];
+
+const entryReducer = (state, action) => {
+    
+    state = state || initialEntryState;
+    
+    if (action.type === actions.ADD_ENTRY) {
+        return state.concat({
+            type: action.entry,
+            description: null,
+        });
+fetch(url)
         .then( res => res.json() )
-        .then( data => state=Object.assign(state, data));
-    return state;
+        .then( data => state=Object.assign(state, data));    
+
+const allReducers = combineReducers({
+    energyData: reducer
+/* var newRepository = Object.assign({}, repository, {
+            description: 'N/A' 
+        });   */
+})
+  
+    
+    
+    
+   return before.concat(newEntry, after);
+
+
 };
 
-var allReducers = combineReducers({
-    energyData: reducer
-});
+return state;
+};
 
-export default allReducers;
+// export default allReducers;
+exports.entryReducer = entryReducer;
